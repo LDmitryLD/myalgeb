30cd88294ca8330fa3f687ce2085db9d4795f5b4
Dmirty <dima.lednev@mail.ru>
2023-10-17 23:29:17 +0300

    myalgebv

--- myalgeb.go
+++ myalgeb.go
@@ -14,3 +14,23 @@ func Acosh(x float64) float64 {
 func Asin(x float64) float64 {
 	return math.Asin(x)
 }
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
